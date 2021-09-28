e60e68081b3cc572406ef3dea4ca9674b792ad66
Marcelo Sampaio <marcelosampaio1965@gmail.com>
2021-09-28 16:26:03 -0300

    Initial Commit

---
+++ CustomSegmentedControl/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  CustomSegmentedControl
+//
+//  Created by Marcelo Sampaio on 28/09/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
